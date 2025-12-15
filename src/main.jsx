a3a3e7c12decd095a522562f5f460e68af9f5db5
alexmonarca <alex@agenciamonarca.com.br>
2025-12-15 06:04:33 -0300

    Setup React application with main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// IMPORTAÇÃO DO SEU COMPONENTE:
+import App from './ChatwootCRMSimulator.jsx'; 
+// Necessário para o Tailwind CSS ser aplicado
+import './index.css'; 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
 
